d6c5281e2f31584322e03f8fc4127e90db3b4ad4
laruskonrad <laruskonrad@hotmail.com>
2015-10-15 14:04:54 +0000

    copy fra hannesi

--- src/test/java/is/ru/stringcalculator/CalculatorTest.java
+++ src/test/java/is/ru/stringcalculator/CalculatorTest.java
@@ -6,7 +6,7 @@ import org.junit.Test;
 public class CalculatorTest {
 
 	public static void main(String args[]) {
-      org.junit.runner.JUnitCore.main("is.ru.stringcalculator.CalculatorTest");
+      org.junit.runner.JUnitCore.main("is.ru.calculator.CalculatorTest");
     }
 
 	@Test
