4e8c030a73ce5819dab3f450d9b5c663df69e206
laruskonrad <laruskonrad@hotmail.com>
2015-10-17 12:53:43 +0000

    Tested lotsOfNumbers

--- src/test/java/is/ru/stringcalculator/CalculatorTest.java
+++ src/test/java/is/ru/stringcalculator/CalculatorTest.java
@@ -29,4 +29,9 @@ public class CalculatorTest {
     	assertEquals(6, Calculator.add("1,2,3"));
     }
 
+    @Test
+    public void testLotsOfNumber(){
+    	assertEquals(15, Calculator.add("0,1,2,3,4,5"));
+    }
+
 }
